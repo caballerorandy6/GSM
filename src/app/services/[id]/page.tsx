db6a23dc457d09be3e765e2f31adc3e0717296d5
Randy Caballero <112903057+caballerorandy6@users.noreply.github.com>
2025-05-06 02:23:52 -0500

    updated servige page component

--- src/app/services/[id]/page.tsx
+++ src/app/services/[id]/page.tsx
@@ -1,12 +1,6 @@
 import ServiceClient from "@/components/ServiceClient";
 
-interface Props {
-  params: {
-    id: string;
-  };
-}
-
-export default function ServicePage({ params }: Props) {
+export default function ServicePage({ params }: { params: { id: string } }) {
   return (
     <main className="p-8">
       <ServiceClient id={params.id} />
